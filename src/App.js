711a5f202223f33aa339bed316fbc6a60e595b6f
max <escriboy@icloud.com>
2024-07-08 22:25:22 +0900

    simple todo

--- src/App.js
+++ src/App.js
@@ -1,32 +1,34 @@
-import { useEffect, useState } from 'react';
-
-function Hello() {
-  // function byFn() {
-  //   console.log('bye :(');
-  // }
-  // function hiFn() {
-  //   console.log('Created :)');
-  //   return byFn;
-  // }
-
-  // useEffect(hiFn, []);
-
-  useEffect(() => {
-    console.log('hi :)');
-    return () => console.log('bye :(');
-  });
-
-  return <h1>Hello</h1>;
-}
+import { useState } from 'react';
+import './darkmode.css';
 
 export default function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
-
+  const [toDo, setToDo] = useState('');
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === '') {
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo('');
+  };
   return (
     <div>
-      {showing && <Hello />}
-      <button onClick={onClick}>{showing ? 'Hide' : 'Show'}</button>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type='text'
+          placeholder='Write your to do'
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      {toDos.map((item, index) => (
+        <li key={index}>{item}</li>
+      ))}
     </div>
   );
 }
